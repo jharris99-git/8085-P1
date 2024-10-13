3a7cbdb4a961c57d63a5c3ea57f2d330ffbf36ec
jharris99-git <jharris99@my.bcit.ca>
2024-10-12 17:53:00 -0700

    Fixed column dtypes by replacing misrepresented row values

--- prep.py
+++ prep.py
@@ -1,6 +1,8 @@
+from ast import literal_eval
+
 import pandas as pd
 
-pd.set_option('display.max_rows', 20)
+pd.set_option('display.max_rows', 100)
 pd.set_option('display.max_columns', 100)
 pd.set_option('display.width', 5000)
 
@@ -16,10 +18,28 @@ def final_prep(data):
     label_data.to_csv('./datasets/prepped_label_data.csv', index=True)
 
 
+expected_dtypes = {'srcip': 'str',
+                   'sport': 'str',
+                   'dstip': 'str',
+                   'dsport': 'str',
+                   }
+
 # Import base CSV data from file.
-base_data = pd.read_csv('./datasets/UNSW-NB15-BALANCED-TRAIN.csv', delimiter=',')
+base_data = pd.read_csv('./datasets/UNSW-NB15-BALANCED-TRAIN.csv', dtype=expected_dtypes,
+                        low_memory=False)
 
 
-print(base_data)
+# Fix '-' and '0x' entries in sport and dsport
+base_data.sport = ['00' if x == "-" else x for x in base_data.sport.tolist()]
+base_data.sport = [literal_eval(x) if type(x) == str and x[0:2] == "0x" else int(x) for x in base_data.sport.tolist()]
 
+base_data.dsport = ['00' if x == "-" else x for x in base_data.dsport.tolist()]
+base_data.dsport = [literal_eval(x) if type(x) == str and x[0:2] == "0x" else int(x) for x in base_data.sport.tolist()]
+
+# Fix whitespace values in ct_ftp_cmd, to impute or prune later.
+base_data.ct_ftp_cmd = [None if x == " " else int(x) for x in base_data.ct_ftp_cmd.tolist()]
+
+# Do pruning, imputation, etc.
+
+# Drop redundant columns for Label and attack_cat prediction training data.
 final_prep(base_data)
